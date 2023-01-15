6ef9765e0b8c7703a348036567269fc1bb787ac6
THREEhundo <samuelbaik87@gmail.com>
2023-01-15 15:01:39 -0800

    edit routes added

--- routes/edit.js
+++ routes/edit.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const editController = require('../controllers/edit')
 
-router.get('/', editController.getEdit) // get the edit page with the item id that we want to edit
-router.get('', editController.deleteItem) // deletes the item
-router.post('', editController.updateItem) // updates the item using data passed in from the form
+router.get('/:id', editController.getEdit) // get the edit page with the item id that we want to edit
+router.get('/remove/:id', editController.deleteItem) // deletes the item
+router.post('/update/:id', editController.updateItem) // updates the item using data passed in from the form
 
 module.exports = router
