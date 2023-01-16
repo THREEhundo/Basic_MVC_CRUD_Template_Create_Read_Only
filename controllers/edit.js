264951a3f8ae90eca9ff0f5f3b19c43eee03f88d
THREEhundo <samuelbaik87@gmail.com>
2023-01-15 16:01:23 -0800

    added edit controller methods

--- controllers/edit.js
+++ controllers/edit.js
@@ -5,11 +5,35 @@ module.exports = {
 		const id = req.params.id
 		try {
 			const items = await ItemList.find()
-			res.render('edit.ejs', {itemList: items, idItem: id})
+			res.render('edit.ejs', { itemList: items, idItem: id })
 		} catch (err) {
 			if (err) return res.status(500).send(err)
 		}
 	},
-	deleteItem: async (req,res) => {},
-	updateItem: async (req,res) =>  {}
+	deleteItem: async (req,res) => {
+		const id = req.params.id
+		try {
+			 await ItemList.findByIdAndDelete(id)
+			res.redirect('back')
+		} catch (err) {
+			if (err) return res.status(500).send(err)
+
+		}
+	},
+	updateItem: async (req,res) =>  {
+		try {
+			const id = req.params.id
+			const newnew = await ItemList.findByIdAndUpdate(id, 
+			{
+				textinput: req.body.textinput,
+                numinput: req.body.numinput
+			})
+			console.log(newnew);
+			console.log(ItemList);
+			res.redirect('/')			
+		} catch (err) {
+			if (err) return res.status(500).send(err)
+			res.redirect('/')			
+		}
+	}
 }
